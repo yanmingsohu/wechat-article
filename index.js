6026c971f34bf9a5724732d6c67026ba43087ec9
yanming <yanming-sohu@sohu.com>
2019-04-19 23:30:15 +0800

    Signed-off-by: yanming <yanming-sohu@sohu.com>

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ const yaml = require('js-yaml');
 const fs = require('fs');
 const db = require('./lib/db.js');
 const sv = require('./lib/service.js');
+const sp = require('./lib/sp.js');
 
 const conf = yaml.safeLoad(fs.readFileSync('./config.yaml', 'utf8'));
 
 db.init(conf.mysql);
-proxy.start(conf.proxy_port);
-sv.start(conf.http_port);
+sp.init(function() {
+    proxy.start(conf.proxy_port);
+    sv.start(conf.http_port);
+});
